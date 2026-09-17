7d60214f643bef39110002da40b53b381974c11b
nandinitech-pixel <nandinisuryawanshi133@gmail.com>
2026-09-17 19:15:09 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from supabase import create_client
+
+
+def get_connection():
+    supabase = create_client(
+        st.secrets["SUPABASE_URL"],
+        st.secrets["SUPABASE_KEY"]
+    )
+
+    return supabase
